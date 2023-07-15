d3909d23516ddf1ebfa21a5f8af1e0f6d4b88401
iuryruansc <iuryruansc@gmail.com>
2023-07-15 16:56:36 -0300

    funções extras

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const letter = document.querySelector('#carta-gerada');
 
+// agrupamento das classes css
 const grupos = {
     estilo: ["newspaper", "magazine1", "magazine2"],
     tamanho: ["medium", "big", "reallybig"],
@@ -7,6 +8,7 @@ const grupos = {
     inclinacao: ["skewleft", "skewright"]
 }
 
+// função para criar a carta
 const createLetter = () => {
     const phraseWords = document.querySelector('#carta-texto').value.split(" ");
     const check = phraseCheck(phraseWords);
@@ -16,20 +18,23 @@ const createLetter = () => {
         letter.innerHTML = "";
         for (let index = 0; index < phraseWords.length; index +=1) {
             if (phraseWords[index] === '') {
-        } else {
-            const word = document.createElement('span')
-            word.innerHTML = phraseWords[index];
-            letter.appendChild(word);
-        }
+            } else {
+                const word = document.createElement('span')
+                word.innerHTML = phraseWords[index];
+                letter.appendChild(word);
+            }
         }
     }
     applyStyle();
+    wordCount();
 }
 
+// função para checar se o input está vazio
 const phraseCheck = (array) => {
     return array.every((string) => string.trim() === '');
 }
 
+// função para aplicar css à carta
 const applyStyle = () => {
     const words = document.getElementsByTagName('span');
     for (let index = 0; index < words.length; index += 1) {
@@ -41,8 +46,42 @@ const applyStyle = () => {
     }
 }
 
+// função para alterar o estilo ao clique
+const changeStyle = () => {
+    letter.addEventListener('click', (event) => {
+        if (event.target.classList.value === '') {
+        } else {
+            event.target.classList = [];
+            const randomStyle = grupos.estilo[Math.floor(Math.random() * grupos.estilo.length)]
+            const randomSize = grupos.tamanho[Math.floor(Math.random() * grupos.tamanho.length)]
+            const randomRotation = grupos.rotacao[Math.floor(Math.random() * grupos.rotacao.length)]
+            const randomInclination = grupos.inclinacao[Math.floor(Math.random() * grupos.inclinacao.length)]
+            event.target.classList.add(randomStyle, randomSize, randomRotation, randomInclination);
+        }
+    })
+}
+
+// função para contagem de palavras
+const wordCount = () => {
+    const phraseWords = document.querySelector('#carta-texto').value.split(" ");
+    const check = phraseCheck(phraseWords);
+    var count = 0;
+    if (check) {
+    } else {
+        for (let index = 0; index < phraseWords.length; index +=1) {
+            if (phraseWords[index] === '') {
+            } else {
+                count +=1;
+            }
+        }
+    }
+    document.querySelector('#carta-contador').innerHTML = `A carta contem ${count} palavras`;
+}
+
+// aplicando eventos aos elementos
 const buttonCreate = document.querySelector('#criar-carta');
 buttonCreate.addEventListener('click', createLetter)
 
 window.onload = () => {
+    changeStyle();
 }
