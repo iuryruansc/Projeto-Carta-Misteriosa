94b7b8b59b1fed799a200b0618618ef6a0509baa
iuryruansc <iuryruansc@gmail.com>
2023-07-15 16:02:19 -0300

    aplicando classes css

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 const letter = document.querySelector('#carta-gerada');
 
+const grupos = {
+    estilo: ["newspaper", "magazine1", "magazine2"],
+    tamanho: ["medium", "big", "reallybig"],
+    rotacao: ["rotateleft", "rotateright"],
+    inclinacao: ["skewleft", "skewright"]
+}
+
 const createLetter = () => {
     const phraseWords = document.querySelector('#carta-texto').value.split(" ");
     const check = phraseCheck(phraseWords);
@@ -16,10 +23,22 @@ const createLetter = () => {
         }
         }
     }
+    applyStyle();
 }
 
 const phraseCheck = (array) => {
     return array.every((string) => string.trim() === '');
+}
+
+const applyStyle = () => {
+    const words = document.getElementsByTagName('span');
+    for (let index = 0; index < words.length; index += 1) {
+        const randomStyle = grupos.estilo[Math.floor(Math.random() * grupos.estilo.length)]
+        const randomSize = grupos.tamanho[Math.floor(Math.random() * grupos.tamanho.length)]
+        const randomRotation = grupos.rotacao[Math.floor(Math.random() * grupos.rotacao.length)]
+        const randomInclination = grupos.inclinacao[Math.floor(Math.random() * grupos.inclinacao.length)]
+        words[index].classList.add(randomStyle, randomSize, randomRotation, randomInclination);
+    }
 }
 
 const buttonCreate = document.querySelector('#criar-carta');
